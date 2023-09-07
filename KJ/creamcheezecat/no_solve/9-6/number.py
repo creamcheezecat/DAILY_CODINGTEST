08af0aecde335c8c8f42413ccdf5324bd5483735
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-09-07 10:35:14 +0900

    9/7 문제 풀이

--- KJ/creamcheezecat/no_solve/9-6/number.py
+++ KJ/creamcheezecat/no_solve/9-6/number.py
@@ -1,8 +1,27 @@
 """ 
-
+https://www.acmicpc.net/problem/2212
+사회망 서비스
+2212
 """
 
 import sys
 input = sys.stdin.readline
+import sys
 
+n = int(input())
+k = int(input())
+pos = sorted(list(map(int, input().split())))
 
+if k >= n:
+    print(0)
+    sys.exit()
+
+dist = []
+for i in range(1, n):
+    dist.append(pos[i] - pos[i-1])
+
+dist.sort(reverse=True)
+for _ in range(k-1):
+    dist.pop(0)
+
+print(sum(dist))
---
+++ KJ/creamcheezecat/no_solve/9-7/number.py
@@ -0,0 +1,28 @@
+""" 
+https://www.acmicpc.net/problem/8980
+택배
+8980
+"""
+
+import sys
+input = sys.stdin.readline
+
+N, C =  map(int, input().split())
+M = int(input())
+infos = []
+for _ in range(M):
+    s, r, box = map(int, input().split())
+    infos.append((s, r, box))
+infos.sort(key=lambda x :  x[1])
+
+capa = [C]*N
+total = 0
+for s, r, box in infos:
+    _min = C
+    for i in range(s, r):
+        if _min > min(capa[i], box) :
+            _min = min(capa[i], box)
+    for i in range(s, r):
+        capa[i] -= _min
+    total += _min
+print(total)
