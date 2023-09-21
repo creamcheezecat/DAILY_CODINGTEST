e8036c7beced46d8cbaaf1d6b49a0cb88c17ee7c
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-09-21 22:17:54 +0900

    9/21 문제풀이

---
+++ KJ/creamcheezecat/no_solve/9-21/14791.py
@@ -0,0 +1,28 @@
+""" 
+https://www.acmicpc.net/problem/14791
+깔끔한 숫자
+"""
+
+import sys
+input = sys.stdin.readline
+
+# 깔끔한 수인지 검사
+def isTidy(n):
+    return list(str(n)) == sorted(list(str(n)))
+
+# 가장 가까운 깔끔한 수 구하기
+def check(number):
+    if isTidy(number):
+        return number
+    length = len(str(number))
+    for i in range(1,length):
+        a, b = divmod(number,10**i)
+        target = str(a-1)+'9'*len(str(b))
+        if isTidy(target):
+            return int(target)
+
+# 테스트케이스
+t = int(input())
+for i in range(1,t+1):
+    number = int(input())
+    print(f"Case #{i}:",check(number))
