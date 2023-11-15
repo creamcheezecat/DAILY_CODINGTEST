c348925416771c9c26ba4825694f07cbed33ebef
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-11-15 20:02:03 +0900

    11/15 문제풀이

---
+++ KJ/creamcheezecat/no_solve/11-15/1043.py
@@ -0,0 +1,28 @@
+""" 
+https://www.acmicpc.net/problem/1043
+거짓말
+1043
+"""
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+knowList = set(input().split()[1:])
+parties = []
+
+for _ in range(m):
+    parties.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for party in parties:
+        if party & knowList:
+            knowList = knowList.union(party)
+
+cnt = 0
+for party in parties:
+    if party & knowList:
+        continue
+    cnt += 1
+
+print(cnt)
